728ca9aa11dfdb576fcb95074518205c3b418872
Vlad Ionescu <ionescu.vlad.2010@gmail.com>
2017-01-11 00:44:33 +0200

    Update for all directions
    The new code considers also the diagonals options that are available.

--- main.cpp
+++ main.cpp
@@ -1,70 +1,69 @@
 #include <cstdio>
-#include <cstdlib>
+#include <queue>
 using namespace std;
-
-const int NMAX = 100;
-const int MMAX = 20;
-
-bool Map[NMAX][MMAX], Sol[NMAX][MMAX];
-int posCi, posCj, endCi, endCj;
-int vdi[4] = { 1, 0, -1, 0};
-int vdj[4] = {0, -1, 0, 1};
-int n, m;
-bool inBounds( int posI, int posJ ) {
-	return ( posI >= 0 && posJ >= 0 && posI < n && posJ < m );
-}
-bool notWall( int posI, int posJ ) {
-	return (!Map[posI][posJ] && !Sol[posI][posJ]);
-}
-void finish() {
-	for( int i = 0; i < n; i++ ) {
-		for( int j = 0; j < m; j++ ) {
-			if( Sol[i][j] == true ) {
-				printf( "1 " );
-			} else {
-				printf( "0 " );
-			}
-		}
-		printf( "\n" );
-	}
-	exit( 0 );
-}
-void solve( int pCi, int pCj ) {
-	Sol[pCi][pCj] = true;
-	if( pCi == endCi && pCj == endCj ) {
-		finish();
-	}
-	for( int d = 0; d < 4; d++ ) {
-		if( inBounds( pCi + vdi[d], pCj + vdj[d] ) ) {
-			if( notWall( pCi + vdi[d], pCj + vdj[d] ) ) {
-				solve( pCi + vdi[d], pCj + vdj[d] );
-			}
-		}
-	}
-	Sol[pCi][pCj] = false;
-}
-
+struct Step {
+	int y, x, s;
+} c, t;
+queue<Step> q;
+/**
+ 0  = free
+ -1 = wall
+ -2 = destination
+ 1..= step number
+*/
+int world[1001][1001];
+int dvy[] = {	-1,	-1,	0,	1,	1,	1,	0,	-1};
+int dvx[] = {	0,	1,	1,	1,	0,	-1,	-1,	-1};
 int main() {
-	freopen( "data.txt", "r", stdin );
-	freopen( "sol.txt", "w", stdout );
-	int*x = new int;
-	scanf( "%d%d", &n, &m );
-	posCi = 0;
-	posCj = m - 1;
-	endCi = n - 1;
-	endCj = 0;
-	for( int i = 0; i < n; i++ ) {
-		for( int j = 0; j < m; j++ ) {
-			scanf( "%d", x );
-			if( *x == 1 ) {
-				Map[i][j] = true;
-			} else {
-				Map[i][j] = false;
+	freopen("input.dat", "r", stdin);
+	freopen("output.dat", "w", stdout);
+	int n, m, k, yd, xd, yp, xp, yw, xw, ts = -1;
+	scanf("%d%d%d%d%d%d%d", &n, &m, &k, &yp, &xp, &yd, &xd);
+	world[yd][xd] = -2;
+	c.y = yp;
+	c.x = xp;
+	c.s = 1;
+	for(int i = 0; i < k; i++) {
+		scanf("%d%d", &yw, &xw);
+		world[yw][xw] = -1;
+	}
+	t.s = 1;
+	for(int d = 0; d < 8; d++) {
+		t.y = c.y + dvy[d];
+		t.x = c.x + dvx[d];
+		if(t.y > 0 && t.y <= n &&
+				t.x > 0 && t.x <= m &&
+				world[t.y][t.x] == 0) {
+			world[t.y][t.x] = 1;
+			q.push(t);
+		}
+	}
+	while(!q.empty()) {
+		c = q.front();
+		q.pop();
+		t.s = c.s + 1;
+		for(int d = 0; d < 8; d++) {
+			t.y = c.y + dvy[d];
+			t.x = c.x + dvx[d];
+			if(t.y > 0 && t.y <= n &&
+					t.x > 0 && t.x <= m &&
+					(world[t.y][t.x] == 0 || world[t.y][t.x] > t.s)) {
+				world[t.y][t.x] = t.s;
+				q.push(t);
 			}
 		}
 	}
-	delete x;
-	solve( posCi, posCj );
-	printf("No solution found!");
+	c.y = yd;
+	c.x = xd;
+	for(int d = 0; d < 8; d++) {
+		t.y = c.y + dvy[d];
+		t.x = c.x + dvx[d];
+		if(t.y > 0 && t.y <= n &&
+				t.x > 0 && t.x <= m &&
+				(ts == -1 || world[t.y][t.x] < ts)) {
+			ts = world[t.y][t.x];
+		}
+	}
+	printf("%d", ts + 1);
 	return 0;
 }
